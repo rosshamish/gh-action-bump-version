8cdb0c5a4c4b6332fef427b4cd9a5b50ecc8e07f
Ross Anderson <ross.anderson87@gmail.com>
2021-12-08 00:31:44 -0800

    fix syntax

--- index.js
+++ index.js
@@ -173,8 +173,6 @@ const workspace = process.env.GITHUB_WORKSPACE;
       );
     }
 
-    const do_tag = ;
-    const do_push = ;
     const remoteRepo = `https://${process.env.GITHUB_ACTOR}:${process.env.GITHUB_TOKEN}@github.com/${process.env.GITHUB_REPOSITORY}.git`;
     if (process.env['INPUT_SKIP-TAG'] !== 'true') {
       // skip-tag: false
